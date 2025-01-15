6f3349f254a8141f193a32e14c3ff39363454ea4
levshachin <lewa.shachin@yandex.ru>
2025-01-15 05:49:59 +0300

    fix

--- src/components/ServersList.jsx
+++ src/components/ServersList.jsx
@@ -1,24 +1,25 @@
 import { useState, useEffect } from "react";
 import ServerCard from "./modules/ServerCard";
 
-export default function ServersList(){
+export default function ServersList() {
     const [ServerList, SetServerList] = useState([])
     useEffect(() => {
         const fetchServers = async () => {
-          try {
-            const response = await fetch('https://api.cloudsell.ru/servers/list');
-            const data = await response.json();
-            SetServerList(data);
-            // setFilteredServerList(data);
-          } catch (error) {
-            console.error('Ошибка:', error);
-          }
+            try {
+                const response = await fetch('https://api.cloudsell.ru/servers/list');
+                const data = await response.json();
+                SetServerList(data);
+                // setFilteredServerList(data);
+            } catch (error) {
+                console.error('Ошибка:', error);
+            }
         };
         fetchServers();
-      }, []);
-    return(
+    }, []);
+    console.log(ServerList, ServerList.length)
+    return (
         <div className="ServersList">
-         {ServerList.length!=0?ServerList.map((s)=> {return <ServerCard key={s.id} serverName={s.name} serverPrice={parseInt(s.price)} serverFeature={[s.features.cores, parseInt(s.features.core_frequency).toFixed(1) , s.features.ram, s.features.ram_type, s.features.disk, s.features.disk_type, parseInt(s.features.network_speed)]}/>}):<p>Сервера не найдены!</p>}
+            {ServerList.length != 0 ? ServerList.map((s) => { return <ServerCard key={s.id} serverName={s.name} serverPrice={parseInt(s.price)} serverFeature={[s.features.cores, parseInt(s.features.core_frequency).toFixed(1), s.features.ram, s.features.ram_type, s.features.disk, s.features.disk_type, parseInt(s.features.network_speed)]} /> }) : <p>Сервера не найдены!</p>}
         </div>
     )
-    }
+}
