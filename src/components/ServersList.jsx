2ad5bf05138cad9cf224922a5eb0bb83ccbcc3d5
levshachin <lewa.shachin@yandex.ru>
2025-02-12 16:19:58 +0300

    hui

--- src/components/ServersList.jsx
+++ src/components/ServersList.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import ServerCard from "./modules/ServerCard";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-export default function ServersList({parameters, ...props}) {
+export default function ServersList({ parameters, ...props }) {
     const [ServerList, SetServerList] = useState([])
     console.log('https://api.cloudsell.ru/servers/list?' + parameters)
     useEffect(() => {
         const fetchServers = async () => {
             try {
-                const response = await fetch('https://api.cloudsell.ru/servers/list?' + parameters);
+                const response = await fetch('https://api.cloudsell.ru/pricing-plans/list?' + parameters);
                 const data = await response.json();
                 SetServerList(data);
                 // setFilteredServerList(data);
@@ -19,8 +20,12 @@ export default function ServersList({parameters, ...props}) {
     }, [parameters]);
     console.log(ServerList, ServerList.length)
     return (
+
         <div className="ServersList">
-            {ServerList.length != 0 ? ServerList.map((s) => { return <ServerCard key={s.id} serverName={s.name} serverPrice={parseInt(s.price)} serverFeature={[s.features.cores, parseFloat(s.features.core_frequency).toFixed(1), s.features.ram, s.features.ram_type, s.features.disk, s.features.disk_type, parseInt(s.features.network_speed)]} /> }) : <p style={{justifySelf: "center", alignSelf:'center'}}>Сервера не найдены!</p>}
+            <TransitionGroup>
+                {ServerList.length > 0 ? ServerList.map((s) => { return <CSSTransition key={s.id} timeout={500} classNames="card"><ServerCard serverName={s.name} serverPrice={parseInt(s.price)} serverFeature={[s.features.cores, parseFloat(s.features.core_frequency).toFixed(1), s.features.ram, s.features.ram_type, s.features.disk, s.features.disk_type, parseInt(s.features.network_speed)]} /></CSSTransition> }) : <p style={{ justifySelf: "center", alignSelf: 'center' }}>Сервера не найдены!</p>}
+            </TransitionGroup>
         </div>
+
     )
 }
